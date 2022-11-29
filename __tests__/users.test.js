882bcefb8d66d92f3ed6a44fc1d5563996e63ff2
Adam Robson <adamray312@gmail.com>
2022-11-28 21:41:52 -0800

    remove example from .env npm i

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -36,8 +36,10 @@ describe('user routes', () => {
     pool.end();
   });
 
-  it('creates a new user', async () => {
-    const res = await request(app).post('/api/v1/users').send(mockUser);
+  it('POST / creates a new user', async () => {
+    const res = await request(app)
+      .post('/api/v1/users')
+      .send(mockUser);
     const { firstName, lastName, email } = mockUser;
 
     expect(res.body).toEqual({
@@ -48,55 +50,43 @@ describe('user routes', () => {
     });
   });
 
-  it('signs in an existing user', async () => {
-    await request(app).post('/api/v1/users').send(mockUser);
+  it('POST /sessions signs in an existing user', async () => {
+    await request(app)
+      .post('/api/v1/users')
+      .send(mockUser);
     const res = await request(app)
       .post('/api/v1/users/sessions')
       .send({ email: 'test@example.com', password: '12345' });
     expect(res.status).toEqual(200);
   });
 
-  it('/protected should return a 401 if not authenticated', async () => {
-    const res = await request(app).get('/api/v1/users/protected');
+  it('GET /protected should return a 401 if not authenticated', async () => {
+    const res = await request(app)
+      .get('/api/v1/users/protected');
     expect(res.status).toEqual(401);
   });
 
-  it('/protected should return the current user if authenticated', async () => {
-    const [agent] = await registerAndLogin();
-    const res = await agent.get('/api/v1/users/protected');
+  it('GET /protected should return the current user if authenticated', async () => {
+    const agent = await registerAndLogin();
+    const res = await agent
+      .get('/api/v1/users/protected');
     expect(res.status).toEqual(200);
   });
 
-  it('/users should return 401 if user not admin', async () => {
-    const [agent] = await registerAndLogin();
-    const res = await agent.get('/api/v1/users/');
-    expect(res.status).toEqual(403);
-  });
-
-  it('/users should return 200 if user is admin', async () => {
+  it('GET /users should return 403 if user not admin', async () => {
     const agent = request.agent(app);
-
-    // create a new user
-    await agent.post('/api/v1/users').send({
-      email: 'admin',
-      password: '1234',
-      firstName: 'admin',
-      lastName: 'admin',
-    });
-    // sign in the user
-    await agent
-      .post('/api/v1/users/sessions')
-      .send({ email: 'admin', password: '1234' });
-
-    // const [agent] = await registerAndLogin({ email: 'admin' });
-    const res = await agent.get('/api/v1/users/');
-    expect(res.status).toEqual(200);
+    await UserService.create({ ...mockUser });
+    await agent.post('/api/v1/users/sessions').send({ email: 'Test', password: '12345' });
+    const res = await agent
+      .get('/api/v1/users');
+    expect(res.statusCode).toEqual(403);
   });
 
   it('/users should return a 200 if user is admin', async () => {
-    const [agent] = await registerAndLogin({ email: 'admin' });
-    const res = await agent.get('/api/v1/users/');
-    expect(res.status).toEqual(200);
+    const agent = await registerAndLogin({ email: 'admin', password: '1234' });
+    const res = await agent
+      .getById('/api/v1/users');
+    expect(res.statusCode).toEqual(200);
   });
 
   it('DELETE /sessions deletes the user session', async () => {
