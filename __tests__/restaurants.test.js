5844b4025726cec3562ffd57fbbf06576d0c97a1
Adam Robson <adamray312@gmail.com>
2022-11-29 08:23:19 -0800

    add restaurants test and reviews model

---
+++ __tests__/restaurants.test.js
@@ -0,0 +1,25 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('restaurants routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  afterAll(() => {
+    pool.end();
+  });
+
+  it('GET api/v1/restaurants should return a list of restaurants', async () => {
+    const resp = await request(app).get('/api/v1/restaurants');
+    expect(resp.status).toBe(200);
+    expect(resp.body).toMatchInlineSnapshot();
+  });
+
+  it('GET api/v1/restaurants/:id should return a restaurant with reviews', async () => {
+    const resp = await request(app).get('/api/v1/restaurants/1');
+    expect(resp.status).toBe(200);
+    expect(resp.body).toMatchInlineSnapshot();
+  });
+});
